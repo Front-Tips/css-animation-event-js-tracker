b2b7cf1e1effe144874e9083e99d275727c71787
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2024-01-02 11:51:01 +0100

    Remove commented code

--- src/js/main.js
+++ src/js/main.js
@@ -4,31 +4,3 @@ import "../css/tutorial.css";
 
 /* JS Files */
 import "./tutorial";
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const tuna = document.querySelector(".tuna");
-//   const status = document.getElementById("status");
-//   const replayButton = document.getElementById("replay");
-
-//   /* ------------------------ */
-//   /*      Animation Start     */
-//   /* ------------------------ */
-//   tuna.addEventListener("animationstart", () => {
-//     status.textContent = "Started";
-//   });
-
-//   /* ------------------------ */
-//   /*       Animation End      */
-//   /* ------------------------ */
-//   tuna.addEventListener("animationend", () => {
-//     status.textContent = "Ended";
-//   });
-
-//   // Replay Animation
-//   replayButton.addEventListener("click", function () {
-//     tuna.style.animation = "none";
-//     requestAnimationFrame(function () {
-//       tuna.style.animation = "walk-cycle steps(11) 1s";
-//     });
-//   });
-// });
